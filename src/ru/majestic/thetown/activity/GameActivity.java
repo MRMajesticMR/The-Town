9b98453a7ef8ad6f7984c769d4a1b7125a3f65d3
MRMajesticMR <mrmajesticmr90@gmail.com>
2015-05-25 21:03:43 +0300

    Чистка проекта
    И добавление AndEngine.

---
+++ src/ru/majestic/thetown/activity/GameActivity.java
@@ -0,0 +1,17 @@
+package ru.majestic.thetown.activity;
+
+import ru.majestic.thetown.R;
+import ru.majestic.thetown.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+    
+}
