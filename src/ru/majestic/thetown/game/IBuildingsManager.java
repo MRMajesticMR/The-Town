49523cb37c633b576e55a4fd8b03c63b430d1b9a
MRMajesticMR <mrmajesticmr90@gmail.com>
2015-06-01 20:19:39 +0300

    Добавлено 5 домов (2)

--- src/ru/majestic/thetown/game/IBuildingsManager.java
+++ src/ru/majestic/thetown/game/IBuildingsManager.java
@@ -6,7 +6,12 @@ import android.content.SharedPreferences.Editor;
 
 public interface IBuildingsManager {
 
-   public static final int BUILDING_TYPE_TENT = 0;
+   public static final int BUILDING_TYPE_TENT         = 0;
+   public static final int BUILDING_TYPE_BIG_TENT     = 1;
+   public static final int BUILDING_TYPE_SHACK        = 2;
+   public static final int BUILDING_TYPE_HUT          = 3;
+   public static final int BUILDING_TYPE_LOG_CABIN    = 4;
+   
    
    public void       load                       (SharedPreferences prefs);
    public void       save                       (Editor prefsEditor);
