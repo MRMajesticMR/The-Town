7c44f2db48a7f496e06bc8e1adda85003421b556
MRMajesticMR <mrmajesticmr90@gmail.com>
2015-07-30 12:09:21 +0300

    Изменен внешний вид кликера деревьев

--- src/ru/majestic/thetown/view/clickers/impl/WoodClickerView.java
+++ src/ru/majestic/thetown/view/clickers/impl/WoodClickerView.java
@@ -5,11 +5,11 @@ import ru.majestic.thetown.view.clickers.AClickerViewSkeleton;
 
 public class WoodClickerView extends AClickerViewSkeleton {
 
-   private static final int X = 245;
-   private static final int Y = 500;
+   private static final int X = 235;
+   private static final int Y = 550;
    
-   private static final float WIDTH    = 235.0f;
-   private static final float HEIGHT   = 265.0f;   
+   private static final float WIDTH    = 255.0f;
+   private static final float HEIGHT   = WIDTH * 0.85f;   
    
    public WoodClickerView() {
       super(X, Y, WIDTH, HEIGHT, ResourceManager.getInstance().getWoodClickerBgndTextureRegion(), ResourceManager.getInstance().getEngine().getVertexBufferObjectManager());
